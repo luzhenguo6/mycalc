ed4fb34f11b0aab52ee97d2327d85fe00e44fc98
luzhenguo <luzhenguo6@outlook.com>
2018-06-06 16:45:10 +0800

    词法分析

---
+++ src/main/java/com/lzg/calc/Calculate.java
@@ -0,0 +1,137 @@
+package com.lzg.calc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Calculate {
+
+    public static final int MAX_TOKEN_SIZE = 100;
+
+    public static byte[] st_line;
+    public static int st_line_pos = 0;
+
+    enum TokenKind {
+        BAD_TOKEN,
+        NUMBER_TOKEN,
+        ADD_OPERATOR_TOKEN,
+        SUB_OPERATOR_TOKEN,
+        MUL_OPERATOR_TOKEN,
+        DIV_OPERATOR_TOKEN,
+        END_OF_LINE_TOKEN,
+    }
+
+    enum LexerStatus {
+        INITIAL_STATUS,
+        IN_INT_PART_STATUS,
+        DOT_STATUS,
+        IN_FRAC_PART_STATUS,
+    }
+
+    class Token {
+        TokenKind kind;
+        double value;
+        byte[] str = new byte[MAX_TOKEN_SIZE];
+    }
+
+    void set_line(byte[] line) {
+        st_line = line;
+        st_line_pos = 0;
+    }
+
+    void get_token(Token token) {
+        int out_pos = 0;
+        LexerStatus status = LexerStatus.INITIAL_STATUS;
+        byte current_byte;
+
+        token.kind = TokenKind.BAD_TOKEN;
+        while (st_line[st_line_pos] != 0) {
+            current_byte = st_line[st_line_pos];
+            if ((status == LexerStatus.IN_INT_PART_STATUS
+                    || status == LexerStatus.IN_FRAC_PART_STATUS) &&
+                    !Character.isDigit(current_byte) && current_byte != '.') {
+                token.kind = TokenKind.NUMBER_TOKEN;
+                try {
+                    token.value = Double.valueOf(new String(token.str));
+                } catch (Exception e) {
+                }
+                return;
+            }
+            if (current_byte == '\n') {
+                token.kind = TokenKind.END_OF_LINE_TOKEN;
+                return;
+            }
+            if (Character.isSpaceChar(current_byte)) {
+                st_line_pos++;
+                continue;
+            }
+            if (out_pos >= MAX_TOKEN_SIZE-1) {
+                System.err.println("token too long.");
+                return;
+            }
+            token.str[out_pos] = st_line[st_line_pos];
+            st_line_pos++;
+            out_pos++;
+            token.str[out_pos] = 0;
+
+            if (current_byte == '+') {
+                token.kind = TokenKind.ADD_OPERATOR_TOKEN;
+                return;
+            } else if (current_byte == '-') {
+                token.kind = TokenKind.SUB_OPERATOR_TOKEN;
+                return;
+            } else if (current_byte == '*') {
+                token.kind = TokenKind.MUL_OPERATOR_TOKEN;
+                return;
+            } else if (current_byte == '/') {
+                token.kind = TokenKind.DIV_OPERATOR_TOKEN;
+                return;
+            } else if (Character.isDigit(current_byte)) {
+                if (status == LexerStatus.INITIAL_STATUS) {
+                    status = LexerStatus.IN_INT_PART_STATUS;
+                } else if (status == LexerStatus.DOT_STATUS) {
+                    status = LexerStatus.IN_FRAC_PART_STATUS;
+                }
+            } else if (current_byte == '.') {
+                if (status == LexerStatus.IN_INT_PART_STATUS) {
+                    status = LexerStatus.DOT_STATUS;
+                } else {
+                    System.err.printf("syntax error.\n");
+                    return;
+                }
+            } else {
+                System.err.printf("bad character(%d)\n", current_byte);
+                return;
+//                throw new Exception(String.format("bad character(%d)", current_byte));
+            }
+        }
+    }
+
+    void parse_line(byte[] buf) {
+        Token token;
+        set_line(buf);
+        for (;;) {
+            token = new Token();
+            get_token(token);
+            if (token.kind == TokenKind.END_OF_LINE_TOKEN) {
+                break;
+            } else if (token.kind == TokenKind.BAD_TOKEN) {
+                System.out.printf("kind..(%s), str..%s\n", token.kind, new String(token.str));
+                break;
+            } else {
+                System.out.printf("kind..(%s), str..%s\n", token.kind, new String(token.str));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Calculate calculate = new Calculate();
+
+        char[] buf = new char[1024];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (br.read(buf) > 0) {
+            calculate.parse_line(new String(buf).getBytes());
+        }
+    }
+
+}
